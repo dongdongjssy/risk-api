398d8c80f61d909c439ac86202556a0307403447
Dong Dong <ddong@nanthealth.com>
2024-09-28 19:54:15 -0400

    delete http client test files

--- utils/validate_state.go
+++ utils/validate_state.go
@@ -2,6 +2,7 @@ package utils
 
 var riskStates = [4]string{"open", "closed", "accepted", "investigating"}
 
+// validate if given state is valid
 func IsValidState(state string) bool {
 	for _, val := range riskStates {
 		if val == state {
