9fdf2e477c552aaa5edbc076b1e3337f56142b04
Dong Dong <ddong@nanthealth.com>
2024-09-28 19:16:45 -0400

    remove authentication middleware that has no implementations in it

--- middlewares/authentication.go
+++
@@ -1,8 +0,0 @@
-package middlewares
-
-import "github.com/gin-gonic/gin"
-
-func Authenticate(ctx *gin.Context) {
-	// TODO: authentication logic
-	ctx.Next()
-}
--- routes/risk_routes.go
+++ routes/risk_routes.go
@@ -2,16 +2,11 @@ package routes
 
 import (
 	"github.com/dongdongjssy/risk-api/handlers"
-	"github.com/dongdongjssy/risk-api/middlewares"
 	"github.com/gin-gonic/gin"
 )
 
 func RegisterRiskRoutes(server *gin.Engine) {
-	riskApis := server.Group("/")
-
-	// assume the apis can be used by only valid users or services
-	riskApis.Use(middlewares.Authenticate)
-	riskApis.GET("/v1/risks", handlers.GetRisks)
-	riskApis.GET("/v1/risks/:id", handlers.GetRisk)
-	riskApis.POST("/v1/risks", handlers.CreateRisk)
+	server.GET("/v1/risks", handlers.GetRisks)
+	server.GET("/v1/risks/:id", handlers.GetRisk)
+	server.POST("/v1/risks", handlers.CreateRisk)
 }
