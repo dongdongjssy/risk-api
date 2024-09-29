a752c9186ec253a6878723dbb68f8e807c9d3711
Dong Dong <ddong@nanthealth.com>
2024-09-29 11:38:34 -0400

    clean code

--- main.go
+++ main.go
@@ -11,11 +11,6 @@ import (
 // @license.url http://www.apache.org/licenses/LICENSE-2.0.html
 // @BasePath /v1
 func main() {
-	// save logging to a local file that's in the same folder where the program runs
-	// gin.DisableConsoleColor()
-	// file, _ := os.Create("risk-api.log")
-	// gin.DefaultWriter = io.MultiWriter(file)
-
 	server := gin.Default()
 	routes.RegisterRiskRoutes(server)
 	server.Run("localhost:8080")
